df586975d516014e07022d06b5815e6254a0133b
Linards <9407839+llinards@users.noreply.github.com>
2024-03-10 08:54:39 +0200

    Project setup, components created

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css',
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a description',
+      'https://picsum.photos/1024/800'
+    ),
+  ];
+}
